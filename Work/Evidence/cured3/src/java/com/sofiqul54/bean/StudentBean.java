b00f16aac93458cb43801aa838a82e415669dcf2
sofiqul-54 <sofiqul54@gmail.com>
2019-02-02 11:50:09 +0600

    primeface cured3

---
+++ Work/Evidence/cured3/src/java/com/sofiqul54/bean/StudentBean.java
@@ -0,0 +1,77 @@
+package com.sofiqul54.bean;
+
+import com.sofiqul54.entity.Student;
+import com.sofiqul54.service.StudentService;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class StudentBean {
+    
+    private StudentService studentService = new StudentService();
+    private Student student;
+    private List<Student> list;
+    private String[] completedCourses;
+    
+    public StudentService getStudentService() {
+        return studentService;
+    }
+    
+    public void setStudentService(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    
+    public Student getStudent() {
+        if (student == null) {
+            student = new Student();
+        }
+        return student;
+    }
+    
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+    
+    public List<Student> getList() {
+        list = new ArrayList<>();
+        list = studentService.getList();
+        return list;
+    }
+    
+    public void setList(List<Student> list) {
+        this.list = list;
+    }
+    
+    public String[] getCompletedCourses() {
+        return completedCourses;
+    }
+    
+    public void setCompletedCourses(String[] completedCourses) {
+        this.completedCourses = completedCourses;
+    }
+    
+    public void reset() {
+        student = new Student();
+    }
+    
+    public void save() {
+        String cc = "";
+        for (String s : completedCourses) {
+            cc += s + " ,";
+        }
+        student.setCourses(cc);
+        studentService.saveOrUpdate(student);
+    }
+    
+    public void edit() {
+        studentService.saveOrUpdate(student);
+    }
+    
+    public void delete() {
+        studentService.delete(student);
+    }
+    
+}
